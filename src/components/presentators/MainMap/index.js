43eec72c4249ebcd817d9d11733feee2085f895f
yanagiya <extendcrisis@gmail.com>
2017-08-18 19:44:23 +0900

    Add MainMap

--- src/components/presentators/MainMap/index.js
+++ src/components/presentators/MainMap/index.js
@@ -12,13 +12,13 @@ export default class MainMap extends Component {
   }
   
   render() {
-    const path = "../src/components/presentators/MainMap/worldatlas_800.gif";
     const styles = this.getStyles();
     return (
       <div>
         <input type="text" style={this.props.style}/>
         <input type="button" value="Search"/>
-        <img src={path} style={this.props.style}/>
+        <div id="regions_div" style={styles.map}>
+		</div>
       </div>
     );
   }
